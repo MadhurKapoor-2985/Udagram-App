c46888eb978d2bbcdcc055e447945de67aaf900d
Madhur Kapoor <madhur.kapoor@gmail.com>
2019-08-10 12:27:16 +0000

    Used AWS Secrets Manager to read client secret

--- src/lambda/auth/authOAuthorizer.ts
+++ src/lambda/auth/authOAuthorizer.ts
@@ -1,14 +1,20 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult, CustomAuthorizerHandler } from 'aws-lambda'
 import 'source-map-support/register';
+import * as AWS from 'aws-sdk'
 
 import { verify } from 'jsonwebtoken'
 import { JwtToken } from '../../auth/JwtToken'
 
-const auth0Secret = process.env.AUTH_0_SECRET
+const secretId = process.env.AUTH_0_SECRET_ID
+const secretField = process.env.AUTH_0_SECRET_FIELD
+
+const client = new AWS.SecretsManager()
+
+let cachedSecret: string
 
 export const handler: CustomAuthorizerHandler= async( event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
     try {
-        const decodedToken = verifyToken(event.authorizationToken)
+        const decodedToken = await verifyToken(event.authorizationToken)
         console.log('User was authorized')
 
         return {
@@ -45,7 +51,7 @@ export const handler: CustomAuthorizerHandler= async( event: CustomAuthorizerEve
     }
 }
 
-function verifyToken(authHeader: string): JwtToken {
+async function verifyToken(authHeader: string): Promise<JwtToken> {
     if(!authHeader) {
         throw new Error('No authorization token')
     }
@@ -57,5 +63,20 @@ function verifyToken(authHeader: string): JwtToken {
     const split = authHeader.split(' ')
     const token = split[1]
 
-    return verify(token, auth0Secret) as JwtToken
+    const secretObject: any = await getSecret()
+    const secret = secretObject[secretField]
+
+    return verify(token, secret) as JwtToken
+}
+
+async function getSecret() {
+  if(cachedSecret) return cachedSecret
+
+  const data = await client.getSecretValue({
+    SecretId: secretId
+  }).promise()
+
+  cachedSecret = data.SecretString
+
+  return JSON.parse(cachedSecret)
 }
