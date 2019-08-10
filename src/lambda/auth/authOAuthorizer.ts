befe617ae043499c5fd2ba4edb001ccc11416fc3
Madhur Kapoor <madhur.kapoor@gmail.com>
2019-08-10 11:10:09 +0000

    Added JsonWebToken authentication with Auth0 Service

--- src/lambda/auth/authOAuthorizer.ts
+++ src/lambda/auth/authOAuthorizer.ts
@@ -1,13 +1,18 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult, CustomAuthorizerHandler } from 'aws-lambda'
 import 'source-map-support/register';
 
+import { verify } from 'jsonwebtoken'
+import { JwtToken } from '../../auth/JwtToken'
+
+const auth0Secret = process.env.AUTH_0_SECRET
+
 export const handler: CustomAuthorizerHandler= async( event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
     try {
-        verifyToken(event.authorizationToken)
+        const decodedToken = verifyToken(event.authorizationToken)
         console.log('User was authorized')
 
         return {
-            principalId: 'user',
+            principalId: decodedToken.sub,
             policyDocument: {
               Version: '2012-10-17',
               Statement: [
@@ -40,7 +45,7 @@ export const handler: CustomAuthorizerHandler= async( event: CustomAuthorizerEve
     }
 }
 
-function verifyToken(authHeader: string) {
+function verifyToken(authHeader: string): JwtToken {
     if(!authHeader) {
         throw new Error('No authorization token')
     }
@@ -52,7 +57,5 @@ function verifyToken(authHeader: string) {
     const split = authHeader.split(' ')
     const token = split[1]
 
-    if(token !== '123') {
-        throw new Error('Invalid token')
-    }
+    return verify(token, auth0Secret) as JwtToken
 }
